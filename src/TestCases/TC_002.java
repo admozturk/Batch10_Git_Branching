225d72e6ba73b8e6075ecdf59e678bb60d7e054f
Adem OZTURK <admoztrk86@gmail.com>
2022-12-08 00:47:11 +0100

    new method added

--- src/TestCases/TC_002.java
+++ src/TestCases/TC_002.java
@@ -3,6 +3,8 @@ package TestCases;
 public class TC_002 {
     // Test case 2
 
-    // there will be new features in thi sest case sheet
+    // there will be new features in this test case sheet
+
+    // We will be doing smoke testing in this section
 
 }
